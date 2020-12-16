34a04f1cc57fcbeb107bce494419f4bd426b53bb
jiangsifang <691887420@qq.com>
2020-12-16 09:03:26 +0800

    刘根宏第一次

---
+++ javaproject/src/com/nnxy/team5/Liugenhong_3.java
@@ -0,0 +1,20 @@
+package com.nnxy.team5;
+
+public class Liugenhong_3 {
+
+	
+
+		private int id;
+		
+		
+		public int getid()
+		{
+			return id;
+		}
+		
+		public void setid(int id){
+			this.id=id;
+		}
+	
+
+}
