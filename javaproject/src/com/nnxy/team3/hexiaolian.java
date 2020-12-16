310d1e1b03af7cddd7ea76f2cc5fb418b7aeab0e
love.sang <1522580013@qq.com>
2020-12-16 09:13:50 +0800

    hhhhh

--- javaproject/src/com/nnxy/team3/hexiaolian.java
+++ javaproject/src/com/nnxy/team3/hexiaolian.java
@@ -1,10 +1,37 @@
 package com.nnxy.team3;
 
 public class hexiaolian {
+	private int id;
+	private String name;
+	private int age;
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		
+	}
 
+	public int getId() {
+		return id+10;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 }
