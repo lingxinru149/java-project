fe2b25bf6fce570c547eb60198b89c139bb62bc1
jiangsifang <691887420@qq.com>
2020-12-16 09:05:20 +0800

    1

---
+++ javaproject/src/com/nnxy/team100/tanxiaohao.java
@@ -0,0 +1,27 @@
+package com.nnxy.team100;
+
+public class tanxiaohao {
+	private int qqhao;
+	private int id;
+	private int age;
+	public int getQqhao() {
+		return qqhao;
+	}
+	public void setQqhao(int qqhao) {
+		this.qqhao = qqhao;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
